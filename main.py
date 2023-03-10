14067b152f35b448163488cebbb8a1f492ae1639
Arthur-Bamberg <bamberguisses@gmail.com>
2023-03-10 16:12:23 -0300

    Created functional route for give audio file (not tested)

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
-from SpeechRecognizer import SpeechRecognizer
-
-sr = SpeechRecognizer()
-
-text = sr.recognize_from_mic()
-print(text)
-
-
 from flask import Flask, request
-import requests
+from SpeechRecognizer import SpeechRecognizer
 
 app = Flask(__name__)
 
-# Define the ChatGPT API endpoint
-chatgpt_endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+recognizer = SpeechRecognizer()
 
-# Define your OpenAI API key
-openai_key = "YOUR_OPENAI_API_KEY"
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    # Get the user's message from the request data
-    message = request.form['message']
+@app.route('/audio-to-text', methods=['POST'])
+def audio_to_text():
+    # Get the audio file from the request
+    audio_file = request.files['audio']
 
-    # Set the request headers
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {openai_key}'
-    }
+    # Convert the audio to text using the SpeechRecognizer object
+    text = recognizer.recognize_from_file(audio_file)
 
-    # Set the request data
-    data = {
-        'prompt': f'{message}\nAI:',
-        'temperature': 0.7,
-        'max_tokens': 50
-    }
-
-    # Send a POST request to the ChatGPT API endpoint
-    response = requests.post(chatgpt_endpoint, headers=headers, json=data)
-
-    # Get the AI's response from the API response
-    ai_response = response.json()['choices'][0]['text'].strip()
-
-    # Return the AI's response as the API response
-    return {'message': ai_response}
-
-if __name__ == '__main__':
-    app.run()
+    # Return the text as a response
+    return text
